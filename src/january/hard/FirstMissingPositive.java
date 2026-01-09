00135e8f4eab40be94c98e2811df72e3b3985d06
anton <antonlenz508@gmail.com>
2026-01-09 23:58:30 +0100

    update

--- src/january/hard/FirstMissingPositive.java
+++ src/january/hard/FirstMissingPositive.java
@@ -1,4 +1,23 @@
 package january.hard;
 
+import java.util.HashSet;
+
 public class FirstMissingPositive {
+    public int firstMissingPositive(int[] nums) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums){
+            set.add(num);
+        }
+
+        for (int i = 1 ; i <= nums.length+1 ; i++){
+            if (!set.contains(i)){
+                return i;
+            }
+        }
+
+        return -1;
+
+    }
 }
