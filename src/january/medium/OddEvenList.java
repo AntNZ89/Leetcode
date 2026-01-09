2db6f8deaa83ee13db93824950250a792da6579f
anton <antonlenz508@gmail.com>
2026-01-10 00:02:57 +0100

    update

---
+++ src/january/medium/OddEvenList.java
@@ -0,0 +1,32 @@
+package january.medium;
+
+public class OddEvenList {
+//    public ListNode oddEvenList(ListNode head) {
+//
+//        ListNode even = new ListNode();
+//        ListNode odd = new ListNode();
+//        ListNode ans = odd;
+//        ListNode help = even;
+//        int i = 1;
+//
+//        while (head != null){
+//            if (i % 2 == 0){
+//                even.next = head;
+//                even = even.next;
+//            }
+//            else {
+//                odd.next = head;
+//                odd = odd.next;
+//            }
+//            i++;
+//            head = head.next;
+//        }
+//
+//        even.next = null;
+//        ans = ans.next;
+//        odd.next = help.next;
+//
+//        return ans;
+//
+//    }
+}
