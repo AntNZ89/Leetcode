90aa1bda92db5396c9c2cf2cfcf3d04b891c89f3
anton <antonlenz508@gmail.com>
2026-01-24 12:14:07 +0100

    update

---
+++ src/january/medium/MinPairSum.java
@@ -0,0 +1,17 @@
+package january.medium;
+
+public class MinPairSum {
+    public int minPairSum(int[] nums) {
+
+        int n = nums.length;
+        int ans = 0;
+        Arrays.sort(nums);
+
+        for (int i = 0 ; i < n/2 ; i++){
+            ans = Math.max(ans, nums[i] + nums[n-i-1]);
+        }
+
+        return ans;
+
+    }
+}
