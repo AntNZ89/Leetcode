026e18caff11290b18c98ba642a74cb09b9a8461
anton <antonlenz508@gmail.com>
2026-02-04 16:29:33 +0100

    update

---
+++ src/febuary/medium/FindKthLargestElement.java
@@ -0,0 +1,20 @@
+package febuary.medium;
+
+import java.util.PriorityQueue;
+
+public class FindKthLargestElement {
+    public int findKthLargest(int[] nums, int k) {
+
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+
+        for (int num : nums){
+            q.offer(num);
+            if (q.size() > k){
+                q.poll();
+            }
+        }
+
+        return q.poll();
+
+    }
+}
