fff4bae208ff8ac91c77ef4ef2e2b6e6a8e70e9f
anton <antonlenz508@gmail.com>
2026-02-09 22:58:11 +0100

    update

---
+++ src/febuary/medium/GroupAnagrams.java
@@ -0,0 +1,34 @@
+package febuary.medium;
+
+public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        Map<String, ArrayList<String>> map = new HashMap<>();
+
+        for (String s : strs){
+            char[] tChar = s.toCharArray();
+            Arrays.sort(tChar);
+            String tString = new String(tChar);
+
+            if (map.containsKey(tString)){
+                ArrayList<String> temp = map.get(tString);
+                temp.add(s);
+                map.put(tString, temp);
+            }
+            else {
+                ArrayList<String> temp = new ArrayList<>();
+                temp.add(s);
+                map.put(tString, temp);
+            }
+        }
+
+        List<List<String>> ans = new ArrayList<>();
+
+        for (ArrayList<String> val : map.values()){
+            ans.add(val);
+        }
+
+        return ans;
+
+    }
+}
